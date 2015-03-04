b16d01c1dec6f48c7bdd7bfb5654e5bfa4b34d9e
Tom Coakes <tomcoakes@gmail.com>
2015-03-04 16:34:30 +0000

    Moved script tags into head, and added JQuery ready function to interface code

--- src/Interface.js
+++ src/Interface.js
@@ -1,53 +1,57 @@
-    var thermostat = new Thermostat();
+$(document).ready(function() {
 
+  var thermostat = new Thermostat();
+
+  $("#current_temp").text(function() {
+    return thermostat.temp;
+  });
+
+  $("#increase").click(function() {
+    checkPowerSave();
+    thermostat.changeTemperature(1);
+    updateTemp();
+  });
+
+  $("#decrease").click(function() {
+    thermostat.changeTemperature(-1);
+    updateTemp();
+  });
+
+  $("#reset").click(function() {
+    thermostat.tempReset();
+    updateTemp();
+  });
+
+  $("#powersave").click(function() {
+    checkPowerSave();
+    updateTemp();
+  });
+
+  updateTemp = function() {
     $("#current_temp").text(function() {
       return thermostat.temp;
     });
+    tempColor();
+  };
 
-    $("#increase").click(function() {
-      checkPowerSave();
-      thermostat.changeTemperature(1);
-      updateTemp();
-    });
+  tempColor = function() {
+    if(thermostat.energyUsage() == "high") {
+      $("#current_temp").css("color", "red");
+    }
+    if(thermostat.energyUsage() == "medium") {
+      $("#current_temp").css("color", "orange");
+    }
+    if(thermostat.energyUsage() == "low") {
+      $("#current_temp").css("color", "green");
+    }
+  };
 
-    $("#decrease").click(function() {
-      thermostat.changeTemperature(-1);
-      updateTemp();
-    });
+  checkPowerSave = function() {
+    if ($('#powersave').is(':checked')) {
+      thermostat.powerSavingMode(true);
+    } else {
+      thermostat.powerSavingMode(false);
+    }
+  };
 
-    $("#reset").click(function() {
-      thermostat.tempReset();
-      updateTemp();
-    });
-
-    $("#powersave").click(function() {
-      checkPowerSave();
-      updateTemp();
-    });
-
-    updateTemp = function() {
-      $("#current_temp").text(function() {
-        return thermostat.temp;
-      });
-      tempColor();
-    };
-
-    tempColor = function() {
-      if(thermostat.energyUsage() == "high") {
-        $("#current_temp").css("color", "red");
-      }
-      if(thermostat.energyUsage() == "medium") {
-        $("#current_temp").css("color", "orange");
-      }
-      if(thermostat.energyUsage() == "low") {
-        $("#current_temp").css("color", "green");
-      }
-    };
-
-    checkPowerSave = function() {
-      if ($('#powersave').is(':checked')) {
-        thermostat.powerSavingMode(true);
-      } else {
-        thermostat.powerSavingMode(false);
-      }
-    };
+});
