597b732aab393888f6f62caf28a6e9b19c60ecb6
Tom Coakes <tomcoakes@gmail.com>
2015-03-04 11:23:49 +0000

    Power saving mode created

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -9,13 +9,30 @@ describe("Thermostat", function() {
   });
 
   it("it can have the temperature increased", function() {
-    thermostat.change(5)
-    expect(thermostat.temp).toBe(25)
+    thermostat.change(5);
+    expect(thermostat.temp).toBe(25);
   });
 
    it("it can have the temperature lowered", function() {
-    thermostat.change(-5)
-    expect(thermostat.temp).toBe(15)
+    thermostat.change(-5);
+    expect(thermostat.temp).toBe(15);
   });
+
+   it("cannot go lower than 10 degrees", function() {
+    thermostat.change(-15);
+    expect(thermostat.temp).toBe(10);
+   });
+
+   it("cannot go higher than 32 degrees", function() {
+    thermostat.powerSavingMode(false);
+    thermostat.change(20);
+    expect(thermostat.temp).toBe(32);
+   });
+
+   it("cannot go higher than 25 if power saving is on", function() {
+    thermostat.powerSavingMode(true);
+    thermostat.change(20);
+    expect(thermostat.temp).toBe(25);
+   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,24 @@
 var Thermostat = function() {
   this.temp = 20;
+  this.powerSaving = true;
 };
 
   Thermostat.prototype.change = function(degrees) {
     this.temp += degrees;
+    this._tempControl();
+  };
+
+  Thermostat.prototype.powerSavingMode = function(mode) {
+    mode == false ? this.powerSaving = false : this.powerSaving = true;
+  };
+
+  Thermostat.prototype._tempControl = function() {
+    if (this.temp < 10) { this.temp = 10 };
+    
+    if (this.powerSaving == true) {
+      (this.temp > 25) ? this.temp = 25 : this.temp;
+    } else {
+      (this.temp > 32) ? this.temp = 32 : this.temp;
+    };
+
   };
